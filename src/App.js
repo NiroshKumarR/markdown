0f3f92a489c9dacb4a60dac4a2f418cb2d2fd939
niroshkumar072 <niroshkumar072@.com>
2021-12-12 04:29:14 +0530

    markdown editor css and markdown preview is done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { marked } from "marked";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [markdown, setMarkdown] = useState("#sup Bro");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="title">Markdown Editor</h1>
+      <div className="container">
+        <div className="textarea">
+          <textarea
+            value={markdown}
+            onChange={(e) => setMarkdown(e.target.value)}
+          />
+        </div>
+        <div
+          className="preview"
+          dangerouslySetInnerHTML={{ __html: marked(markdown) }}
+        ></div>
+      </div>
     </div>
   );
 }
-
-export default App;
